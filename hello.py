7ca0288e03f0a79640c6b522410931af9b5847d0
Sterling Davis <sterlingdayvis@gmail.com>
2023-04-07 10:45:44 -0700

    Add files via upload
    Generated OpenAI API key

--- hello.py
+++ hello.py
@@ -1,24 +1,46 @@
 import requests
 import os
-import openai
 import creds
 from tkinter import filedialog
 from tkinter import *
 
-url = requests.get("https://api.openai.com/v1/chat/completions")
+#url = f"https://api.openai.com/v1/chat/completions"
 
-prompt = input("Hello! Welcome to Coverletter! Please press the Enter key to begin.")
+
+import openai
+
+# Getting error message when utilizing API key from OpenAI
+
+openai.api_key = "{{SECRET_KEY}}"
+
+try:
+    print(openai.Model.list())
+except Exception as e:
+    print(f"Error: {e}")
+
+#response = requests.get(url)
+
+
+#requests.get("https://api.openai.com/v1/chat/completions")
+
+#f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+
+#prompt = input("Hello! Welcome to Coverletter! Please press the Enter key to upload your resume.")
 
 # create a Tkinter window
-root = Tk()
-root.withdraw() # hide the root window
+#root = Tk()
+#root.withdraw() # hide the root window
 
 # ask the user to select a file
-file_path = filedialog.askopenfilename()
+#file_path = filedialog.askopenfilename()
 
 # print the file path
-print("Selected file:", file_path)
+#print("Selected file:", file_path)
 
+
+#url = input("Enter website URL: ")
+
+#response = requests.get(url)
 
 
 
